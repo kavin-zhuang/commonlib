06b333026c1690d35baf5e5232b072d4b9a3ca95
jinfeng <kavin.zhuang@gmail.com>
2016-06-14 14:49:05 +0800

    修复无法显示第一行或最后一行的BUG 修复如果参数BUFFER=NULL时，终端打印换行的BUG

--- hexshow.c
+++ hexshow.c
@@ -1,31 +1,66 @@
+/*
+ * Copyright (C) 2016, Jinfeng Zhuang
+ *
+ * BUG FIX:
+ *   1. show line-1
+ * */
+
 #include <stdio.h>
+
+#define SHOW(m, arg...) printf(m, ##arg)
 
 void hex_show(unsigned char *buffer, unsigned int length)
 {
-    unsigned int row, col, row_bytes;
+    unsigned int row, col, row_bytes, row_total;
+    
+    if(NULL == buffer) {
+    	return;
+    }
 
-    for (row = 0; row < length / 16; row++)
+    // fix bug, the last line should be added
+    row_total = (length % 16) ? (length / 16 + 1) : (length / 16);
+
+    for (row = 0; row < row_total; row++)
     {
-        row_bytes = (row == length / 16) ? (row % 16) : 16;
+        row_bytes = (row == row_total - 1) ? (length % 16) : 16; // fixed
 
-        printf("\n%08X : ", row * 16);
+        SHOW("\n%08X : ", row * 16);
 
-        for (col = 0; col < row_bytes; col++)
+        // fix with blank
+        for (col = 0; col < 16; col++)
         {
-            printf("%02X ", buffer[row * 16 + col]);
+            if (col < row_bytes)
+            {
+                SHOW("%02X ", buffer[row * 16 + col]);
+            }
+            else
+            {
+                SHOW("   ");
+            }
         }
 
-        printf("; ");
+        SHOW("; ");
 
-        for (col = 0; col < row_bytes; col++)
+        // fix with blank
+        for (col = 0; col < 16; col++)
         {
-            if ((buffer[row * 16 + col] >= 32) && (buffer[row * 16 + col] <= 126)) {
-                printf("%c", buffer[row * 16 + col]);
+            if (col < row_bytes)
+            {
+                if ((buffer[row * 16 + col] >= 32) && (buffer[row * 16 + col] <= 126))
+                {
+                    SHOW("%c", buffer[row * 16 + col]);
+                }
+                else
+                {
+                    SHOW(".");
+                }
             }
-            else {
-                printf(".");
+            else
+            {
+                SHOW("   ");
             }
         }
     }
+
     printf("\n");
 }
